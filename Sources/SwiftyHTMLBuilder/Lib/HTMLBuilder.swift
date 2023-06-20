bc21ff5d0c0e6586d453229aac2d355fd2e04f49
yukiny0811 <yukiny0811@gmail.com>
2023-06-20 11:18:01 +0900

    aaa

--- Sources/SwiftyHTMLBuilder/Lib/HTMLBuilder.swift
+++ Sources/SwiftyHTMLBuilder/Lib/HTMLBuilder.swift
@@ -9,10 +9,29 @@ import Foundation
 
 @resultBuilder
 public struct HTMLBuilder {
-    public static func buildBlock(_ components: HTMLBase...) -> [HTMLBase] {
+    public static func buildBlock(_ component: any HTMLBase) -> any HTMLBase {
+        component
+    }
+    public static func buildBlock(_ components: any HTMLBase...) -> [any HTMLBase] {
         components
     }
-    public static func buildOptional(_ components: [HTMLBase]?) -> [HTMLBase] {
-        components ?? []
+    public static func buildBlock(_ components: [any HTMLBase]) -> [any HTMLBase] {
+        components
+    }
+    public static func buildBlock(_ components: [any HTMLBase], _ c2: [any HTMLBase]) -> [any HTMLBase] {
+        components + c2
+    }
+    public static func buildOptional(_ component: [any HTMLBase]?) -> any HTMLBase {
+        if let c = component {
+            return transparentGroup{c}
+        } else {
+            return transparentGroup{[""]}
+        }
+    }
+    public static func buildArray(_ components: [any HTMLBase]) -> any HTMLBase {
+        transparentGroup{components}
+    }
+    public static func buildArray(_ components: [[any HTMLBase]]) -> any HTMLBase {
+        transparentGroup{components.map{v in transparentGroup{v}}}
     }
 }
