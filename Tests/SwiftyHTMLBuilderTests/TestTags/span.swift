7578fcf52b55a77b2f285c4a776c82398be0b8d9
yukiny0811 <yukiny0811@gmail.com>
2024-02-01 21:14:41 +0900

    add span

---
+++ Tests/SwiftyHTMLBuilderTests/TestTags/span.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yuki Kuwashima on 2024/02/01.
+//
+
+@testable import SwiftyHTMLBuilder
+import SnapshotTesting
+import XCTest
+import WebKit
+
+final class Test_span: XCTestCase {
+    func test_span() throws {
+        class TestHTML: HTML {
+            var content: HTMLBlock {
+                p {
+                    span {
+                        text("this is span")
+                    }
+                    text("text")
+                }
+            }
+        }
+        let compiled = TestHTML().compile()
+        let webView = WKWebView()
+        webView.frame = .init(x: 0, y: 0, width: 300, height: 300)
+        webView.loadHTMLString(compiled, baseURL: nil)
+        assertSnapshot(of: webView, as: .image, timeout: 15)
+    }
+}
